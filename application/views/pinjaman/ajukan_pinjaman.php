5cafe4671af8db322ac603af8aa5d1a253f4ed49
ardananjungkusuma <ardananjungkusuma@gmail.com>
2020-10-25 18:38:41 +0700

    add fa

--- application/views/pinjaman/ajukan_pinjaman.php
+++ application/views/pinjaman/ajukan_pinjaman.php
@@ -19,6 +19,7 @@
                                 <input type="hidden" name="id_anggota" value="<?= $this->session->userdata('id_anggota'); ?>">
                                 <label class=" form-control-label">Total Tunai Yang diajukan</label>
                                 <div class="input-group">
+                                    <div class="input-group-addon"><i class="fa fa-money"></i></div>
                                     <input class="form-control" type="number" name="total_pengajuan_pinjaman" id="total_pengajuan_pinjaman" required autofocus>
                                 </div>
                                 <small class="form-text text-muted">ex. 5.000.000</small>
@@ -26,6 +27,7 @@
                             <div class="form-group">
                                 <label class=" form-control-label">Alasan Pinjaman</label>
                                 <div class="input-group">
+                                    <div class="input-group-addon"><i class="fa fa-question"></i></div>
                                     <input class="form-control" type="text" name="alasan_pinjaman" id="alasan_pinjaman" required>
                                 </div>
                                 <small class="form-text text-muted">ex. Untuk Keperluan Modal UMKM .....</small>
